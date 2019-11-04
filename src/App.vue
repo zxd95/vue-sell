a7769ee79626f6c311f1dcc7bf92b3d79b99b6a1
zhangxiaodong <774793337@qq.com>
2019-11-04 22:58:49 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,33 @@
   <div id="app">
     <v-header :seller="seller"></v-header>
     <header-detail></header-detail>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
 <script>
 import VHeader from 'components/v-header/v-header'
 import HeaderDetail from 'components/header-detail/header-detail'
+import Tab from 'components/tab/tab'
 import { getSeller } from 'api'
 
 export default {
   name: 'app',
   components: {
     VHeader,
-    HeaderDetail
+    HeaderDetail,
+    Tab
   },
   data () {
     return {
       seller: {}
+    }
+  },
+  computed: {
+    tabs () {
+      return []
     }
   },
   created () {
@@ -34,4 +44,14 @@ export default {
 }
 </script>
 
-<style lang="stylus"></style>
+<style lang="stylus" scoped>
+#app {
+  .tab-wrapper {
+    position: fixed;
+    top: 136px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+}
+</style>
